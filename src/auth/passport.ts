6de0db067e5e53eea8eefd69e51263d8189e4213
yue002 <pattarapon_wongsin@cmu.ac.th>
2024-10-08 19:43:38 +0700

    add google I forget it

--- src/auth/passport.ts
+++ src/auth/passport.ts
@@ -1,12 +1,14 @@
-import { usersTable } from "@/db/schemas.js";
+import passportIns from "passport";
 import { dbClient } from "@db/client.js";
 import { eq } from "drizzle-orm";
-import passportIns from "passport";
+import { usersTable } from "@db/schemas.js";
 import { local } from "./passportLocal.js";
 import { github } from "./passportOauthGithub.js";
+import { google } from "./passportOauthGoogle.js";
 
 passportIns.use(local);
 passportIns.use("github", github);
+passportIns.use("google", google);
 
 passportIns.serializeUser(function (user, done) {
   done(null, user.id);
